96767677b36691ae5296edef61ae599e9da53a6c
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-07 05:29:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,17 +27,17 @@ new_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
- new_array = ["wow", "I", "am", "really", "learning"]
+ new_array = ["wow", "I", "am", "really", "learning", "arrays"]
  new_array[2]
 end
 
 def retrieve_first_element_from_array(array)
-   new_array = ["wow", "I", "am", "really", "learning"]
+   new_array = ["wow", "I", "am", "really", "learning", "arrays!"]
    new_array[0]
 end
 
 def retrieve_last_element_from_array(array)
-   new_array = ["wow", "I", "am", "really", "learning"]
+   new_array = ["wow", "I", "am", "really", "learning", "arrays!"]
    new_array[-1]
 end
 
