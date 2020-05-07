dde3ff7fbd114863e008736ef538e4ae44af4dd9
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-07 05:28:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+   new_array = ["wow", "I", "am", "really", "learning"]
+   new_array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
